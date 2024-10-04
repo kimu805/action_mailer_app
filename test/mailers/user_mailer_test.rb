1128872a992fd8f68f4f3cc98538f6aa9d08c9fb
kimu805 <kimukimuok805@gmail.com>
2024-10-04 13:39:09 +0900

    Update user_mailer_test.rb

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -2,5 +2,9 @@ require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
   test "welcome" do
+    email = UserMailer.with(
+      to: "igarashi@example.com",
+      name: "igaiga"
+    ).welcome
   end
 end
